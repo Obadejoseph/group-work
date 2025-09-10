471d945d14776e2e47ec84e7aa5d7eaaedd7a90b
Anizoomy <habeebolayemi518@gmail.com>
2025-09-10 14:26:45 +0100

    Created the Model

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 2121;
+const express = require("express")
+const mongoose = require('mongoose')
+
+const app = express();
+app.use(express.json());
+
+// app.use(router) 
+
+const db = process.env.DATABASE_URI
+mongoose.connect(db).then
+    (() => {
+
+        console.log("Connection to DataBase has been established successfully");
+        app.listen(PORT, () => {
+            console.log(`server is runing on Port: ${PORT}`);
+
+        })
+
+
+    }).catch((err) => {
+        console.log("unable to connect to db to " + err);
+
+    })
