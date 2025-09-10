946c5270f45dcd808c7c225a98b16ed522a8a38b
Lanreken <adelugbaolanrewaju@gmail.com>
2025-09-10 12:40:34 +0100

    updated the server file

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
+const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 2121;
-const express = require("express")
-
 
 const app = express();
-// const mongoose = require('mongoose')
 
+app.use(express.json());
 
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Database connection is established");
+    app.listen(PORT, () => {
+      console.log(`server is runing on PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Unable to connect to Database: " + error.message);
+  });
 
-// app.use(express.json());
-
-// app.use(router) 
-
-
-// mongoose.connect(process.env.db).then
-//     (() => {
-
-//         console.log("db connection is established");
-        app.listen(PORT, () => {
-            console.log(`server is runing on port${PORT}`);
-
-        })
-
-
-    // }).catch((err) => {
-    //     console.log("unable to connect to db to " + err);
-
-    // })
+  
