8b225773fadbe15d3febb3c6d04f2d25c62231c1
curve234567 <alaye@gmail.com>
2025-09-10 10:39:38 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 2121;
+const express = require("express")
+
+
+const app = express();
+// const mongoose = require('mongoose')
+
+
+
+// app.use(express.json());
+
+// app.use(router) 
+
+
+// mongoose.connect(process.env.db).then
+//     (() => {
+
+//         console.log("db connection is established");
+        app.listen(PORT, () => {
+            console.log(`server is runing on port${PORT}`);
+
+        })
+
+
+    // }).catch((err) => {
+    //     console.log("unable to connect to db to " + err);
+
+    // })
