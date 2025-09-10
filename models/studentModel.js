93aa27a587e5ab410bbca02553865c1ecfe52005
Lanreken <adelugbaolanrewaju@gmail.com>
2025-09-10 15:35:43 +0100

    added a controller file and added a createStudent endpoint/function

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,37 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema({
-  fullName: {
-    type : String,
-    required: true,
-    trim: true
+const studentSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Male", "Female"],
+      trim: true,
+    },
+    stack: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
   },
-  email: {
-    type : String,
-    required: true,
-    trim: true,
-    unique: true,
-    lowercase: true
-  }, 
-  gender: {
-    type : String,
-    required: true,
-    enum: ["Male", "Female"],
-    trim: true
-  },
-  stack: {
-    type : String,
-    required: true,
-    trim: true
-  },
-  age: {
-    type : Number,
-    required: true
-  },
-}, {
-  timestamp: true
-});
+  {
+    timestamp: true,
+  }
+);
 
-const studentModel = mongoose.model('users', studentSchema)
+const studentModel = mongoose.model("users", studentSchema);
 
-module.exports = studentModel
+module.exports = studentModel;
